43c9bca3d5b50844e0778fd6ae0da23d2a53f4ff
Veerakarn Peerakornworrakitt <v@Veerakarns-MacBook-Air.local>
2025-12-13 00:28:15 +0700

    Finished

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './HomePage.css'; // ต้องสร้างไฟล์ CSS สำหรับจัดรูปแบบ
+import './HomePage.css';
 
 const HomePage = () => {
   return (
